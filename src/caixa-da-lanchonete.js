9e75eeb559485793b54b1ecd78f9b948d978589d
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-08-16 20:31:57 -0300

    Retirando comentarios do codigo

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,18 +1,3 @@
-// ### DESCONTOS E TAXAS
-//  Ok - Pagamento em dinheiro tem 5% de desconto
-//  Ok - Pagamento a crédito tem acréscimo de 3% no valor total
-
-// ### OUTRAS REGRAS
-
-// Ok - Caso item extra seja informado num pedido que não tenha o respectivo item principal, apresentar mensagem "Item extra não pode ser pedido sem o principal".
-// Ok - Combos não são considerados como item principal.
-// Ok - É possível pedir mais de um item extra sem precisar de mais de um principal.
-// Ok - Se não forem pedidos itens, apresentar mensagem "Não há itens no carrinho de compra!"
-// OK - Se a quantidade de itens for zero, apresentar mensagem "Quantidade inválida!".
-// Ok - Se o código do item não existir, apresentar mensagem "Item inválido!"
-// Ok - Se a forma de pagamento não existir, apresentar mensagem "Forma de pagamento inválida!"
-
-
 class CaixaDaLanchonete {
     constructor() {
         this.cardapio = {
